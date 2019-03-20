980439cddf83800cff331971f5f47f285893cc4c
RodrigoRodrigues7 <rodrigo7.carvalho.rodrigues@gmail.com>
2019-03-20 19:35:06 -0300

    Implementado o 'Lazy Loading' para a pagina 'HomePage'

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, IonicPage } from 'ionic-angular';
 
+//Esta classe e o controlador da View 'home.html' | O decorator '@Component' e o que torna essa classe um controller
+@IonicPage()
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
