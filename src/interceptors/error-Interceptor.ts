7994e60f65d03a5911240d5530bc3eceb525ac8a
RodrigoRodrigues7 <rodrigo7.carvalho.rodrigues@gmail.com>
2019-03-25 17:15:57 -0300

    Feito tratamendo de erros 403 no 'Error-Interceptor'

--- src/interceptors/error-Interceptor.ts
+++ src/interceptors/error-Interceptor.ts
@@ -2,8 +2,14 @@ import { Injectable } from "@angular/core";
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { Observable } from "rxjs/Rx";
 
+import { StorageService } from "../services/storage.service";
+
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
+
+    constructor(public storage: StorageService) {
+
+    }
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         console.log("Passou no Interceptor");
@@ -23,8 +29,18 @@ export class ErrorInterceptor implements HttpInterceptor {
             console.log("Erro Detectado pelo Interceptor.");
             console.log(errorObj );
 
+            switch(errorObj.status) {
+                case 403:
+                this.handle_403();
+                break;
+            }
+
             return Observable.throw(errorObj);
         }) as any;
+    }
+
+    handle_403() {
+        this.storage.setLocalUser(null);
     }
 
 }
--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -8,37 +8,43 @@ import { API_CONFIG } from '../../config/api.config';
 
 @IonicPage()
 @Component({
-  selector: 'page-profile',
-  templateUrl: 'profile.html',
+	selector: 'page-profile',
+	templateUrl: 'profile.html',
 })
 export class ProfilePage {
 
-  cliente: ClienteDTO;
+	cliente: ClienteDTO;
 
-  constructor(public navCtrl: NavController, 
-              public navParams: NavParams,
-              public storage: StorageService,
-              public cliService: ClienteService) {
-  }
+	constructor(public navCtrl: NavController,
+		public navParams: NavParams,
+		public storage: StorageService,
+		public cliService: ClienteService) {
+	}
 
-  ionViewDidLoad() {
-    let localUser = this.storage.getLocalUser();
-    if(localUser && localUser.email) {
-      this.cliService.findByEmail(localUser.email)
-      .subscribe(response => {
-        this.cliente = response;
-        this.getProfileImage_If_Exists();
-      },
-      error => {});
-    }
-  }
+	ionViewDidLoad() {
+		let localUser = this.storage.getLocalUser();
+		if (localUser && localUser.email) {
+			this.cliService.findByEmail(localUser.email)
+				.subscribe(response => {
+					this.cliente = response;
+					this.getProfileImage_If_Exists();
+				},
+				error => {
+					if (error.status == 403) {
+						this.navCtrl.setRoot('HomePage');
+					}
+				});
+		} else {
+			this.navCtrl.setRoot('HomePage');
+		}
+	}
 
-  getProfileImage_If_Exists() {
-    this.cliService.getImageFromBucket(this.cliente.id)
-    .subscribe(response => {
-      this.cliente.imgUrl = `${API_CONFIG.bucketBaseUrl}/cp${this.cliente.id}.jpg`
-    },
-    error => {});
-  }
+	getProfileImage_If_Exists() {
+		this.cliService.getImageFromBucket(this.cliente.id)
+			.subscribe(response => {
+				this.cliente.imgUrl = `${API_CONFIG.bucketBaseUrl}/cp${this.cliente.id}.jpg`
+			},
+				error => { });
+	}
 
 }
