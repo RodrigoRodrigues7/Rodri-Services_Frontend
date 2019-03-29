e66eec1c48d16b409cb7f0a3f8d32fbfd0aa7858
RodrigoRodrigues7 <rodrigo7.carvalho.rodrigues@gmail.com>
2019-03-29 18:07:23 -0300

    Implementado o componente 'LoadingController' para mostrar uma mensagem de carregamento na pagina de 'Produto'

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
 
 import { ProdutoDTO } from '../../models/produto.dto';
 import { ProdutoService } from '../../services/domain/produto.service';
@@ -7,43 +7,57 @@ import { API_CONFIG } from '../../config/api.config';
 
 @IonicPage()
 @Component({
-  selector: 'page-produtos',
-  templateUrl: 'produtos.html',
+	selector: 'page-produtos',
+	templateUrl: 'produtos.html',
 })
 export class ProdutosPage {
 
-  items: ProdutoDTO[];
+	items: ProdutoDTO[];
 
-  constructor(
-    public navCtrl: NavController, 
-    public navParams: NavParams,
-    public produtoService: ProdutoService) {
-  }
+	constructor(
+		public navCtrl: NavController,
+		public navParams: NavParams,
+		public produtoService: ProdutoService,
+		public loadingCrtl: LoadingController) {
+	}
 
-  ionViewDidLoad() {
-    let categoriaId = this.navParams.get('categoria_id');
-    this.produtoService.findByCategoria(categoriaId)
-    .subscribe(response => {
-     this.items = response['content'];
-     this.loadImageUrl();
-    },
-    error => {});
-  }
+	ionViewDidLoad() {
+		let categoriaId = this.navParams.get('categoria_id');
+		let loader = this.presentLoading();
 
-  loadImageUrl() {
-    for (var i=0; i<this.items.length; i++) {
-      let item = this.items[i];
-      this.produtoService.getSmallImageFromBucket(item.id)
-      .subscribe(response => {
-        item.imgUrl = `${API_CONFIG.bucketBaseUrl}/prod${item.id}-small.jpg`;
-      },
-      error => {});      
-    }
-  }
+		this.produtoService.findByCategoria(categoriaId)
+			.subscribe(response => {
+				this.items = response['content'];
+				loader.dismiss();
+				this.loadImageUrl();
+			},
+				error => {
+					loader.dismiss();
+				});
+	}
 
-  showDetails(produto_id: string) {
-    //Esse objeto no final do método, significa que eu estou passando um parâmetro para a próxima página
-    this.navCtrl.push('ProdutoDetailPage', {produto_id: produto_id});
-  }
+	loadImageUrl() {
+		for (var i = 0; i < this.items.length; i++) {
+			let item = this.items[i];
+			this.produtoService.getSmallImageFromBucket(item.id)
+				.subscribe(response => {
+					item.imgUrl = `${API_CONFIG.bucketBaseUrl}/prod${item.id}-small.jpg`;
+				},
+					error => { });
+		}
+	}
+
+	showDetails(produto_id: string) {
+		//Esse objeto no final do método, significa que eu estou passando um parâmetro para a próxima página
+		this.navCtrl.push('ProdutoDetailPage', { produto_id: produto_id });
+	}
+
+	presentLoading() {
+		let loader = this.loadingCrtl.create({
+			content: "Aguarde um momento ..."
+		});
+		loader.present();
+		return loader;
+	}
 
 }
